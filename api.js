7cc6166ec5e5eff48d75b0322da551e73ec7a210
Mik-xiaoshuai <you@example.com>
2021-05-15 17:15:53 +0800

    注册接口

--- api.js
+++ api.js
@@ -1,34 +1,37 @@
-const express = require('express')  //引入模块
-const app = express()   //调用express
-const mysql = require('mysql')
-const port=3001   //服务运行的端口
+const express = require("express")
+const mysql = require("mysql")
+const bodyParser = require('body-parser')
 
 
-//设置连接参数
+const app = express()
+const port = 3001
 
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'api2009'
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
-//设置跨域访问
-app.all('*', (req, res, next) => {
+//处理跨域
+app.all("*",function(req,res,next){
     //设置允许跨域的域名，*代表允许任意域名跨域
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin","*");
     //允许的header类型
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-type",);
+    res.header("Access-Control-Allow-Headers","content-type");
     //跨域允许的请求方式
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
     next();
 });
 
 
-//建立连接
-connection.connect();
 
+const connection = mysql.createConnection({
+    host     : 'localhost',         //数据库地址
+    user     : 'root',              //数据库用户名
+    password : 'root',            // 数据库密码
+    database : 'api2009'              // 数据库名
+});
+
+connection.connect()
 
 app.get('/',(req,res)=>{
     console.log(req.query)
@@ -59,8 +62,110 @@ app.post('/user/add',(req,res)=>{
 })
 
 
+// goodslist
+app.get('/goods/list',(req,res)=>{
+    let sql = `select goods_id,goods_name,shop_price,number from p_cart limit 5`
+    connection.query(sql, function (error, results, fields) {
+        res.send(results)
+    });
+})
+//用户注册接口
+app.post('/check/add',(req,res)=>{
+    //接收 post数据
+    let username=req.body.username
+    let useremail=req.body.useremail
+    let mobile=req.body.mobile
+    let pass1=req.body.pass1
+    // console.log(user_name)
+    // console.log(user_email)
+    // console.log(mobile)
+    // console.log(password1)
+    let sql=`insert into p_users(user_name,email,mobile,password) values ("${username}","${useremail}",${mobile},"${pass1}")`
+    console.log(sql)
+    connection.query(sql,function(error,results,fields){
+        console.log(results)
+        console.log("受影响的行数： ",results.affectedRows)
 
-``
+
+        if(results.affectedRows>0){   //以受影响行数判断是否注册成功
+            res.send("注册成功")
+        }else{
+            res.send("注册失败")
+        }
+    });
+})
+
+//检测用户名
+app.get("/check/username",(req,res)=>{
+    console.log(req.query)
+    let sql = `select * from p_users where user_name='${req.query.name}'`
+    console.log("sql: ",sql)
+    connection.query(sql, function (error, results, fields) {
+
+        console.log("数据库记录： ",results)
+
+        if(results.length>0){        //已被占用
+            let response_data = {
+                errno: 40005,
+                msg: "用户名已被占用"
+            }
+            res.send(response_data)
+        }else{           // 可以使用
+            let response_data = {
+                errno: 0,
+                msg: "ok"
+            }
+            res.send(response_data)
+        }
+
+    });
+})
+
+//检查邮箱是否已存在
+app.get("/check/email",(req,res)=>{
+    let email=req.query.email
+    let sql=`select * from p_users where email ='${email}'`
+    console.log("sql",sql);
+    connection.query(sql, function (error, results, fields) {
+        console.log("数据库记录",results)
+        if(results.length>0){
+            let response_dat={
+                error:4006,
+                msg:'邮箱已被占用',
+            }
+            res.send(response_dat) //已被占用
+        }else{
+            let response_dat={
+                error:0,
+                msg:'ok',
+            }
+            res.send(response_dat)  //可以使用
+        }
+    });
+})
+//检查手机号是否已存在
+app.get("/check/mobile",(req,res)=>{
+    let mobile=req.query.mobile
+    let sql=`select * from p_users where mobile ='${mobile}'`
+    console.log("sql",sql);
+    connection.query(sql, function (error, results, fields) {
+        console.log("数据库记录",results)
+        if(results.length>0){
+            let response_dat={
+                error:4006,
+                msg:'手机号已被占用',
+            }
+            res.send(response_dat) //已被占用
+        }else{
+            let response_dat={
+                error:0,
+                msg:'ok',
+            }
+            res.send(response_dat)  //可以使用
+        }
+    });
+})
+
 //监听端口
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
